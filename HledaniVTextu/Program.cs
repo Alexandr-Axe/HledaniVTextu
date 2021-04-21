de0a32d14ffd5a97ba44b3ce3280b3cf5a4370eb
Sekera Alexandr <sekera.alexandr.2017@skola.ssps.cz>
2021-04-21 15:10:05 +0200

    Nefunkční verze (asi potřebná změna)

--- HledaniVTextu/Program.cs
+++ HledaniVTextu/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace HledaniVTextu
 {
@@ -10,7 +11,49 @@ namespace HledaniVTextu
     {
         static void Main()
         {
+            string path = "testovaci.txt";
+            Words.CreateList(path);
+        }
+    }
+    public class Words 
+    {
+        public static void CreateList(string path)
+        {
+            HashSet<string> pouzitaSlova = new HashSet<string>();
+            string[] poleSlov = File.ReadAllLines(path);
+            string[] slovaVRadku;
+            int radek = 0;
+            int zachytRadek = 0;
+            int slovo = 0;
+            string hledaneSlovo = string.Empty;
+            while (radek != poleSlov.Length)
+            {
+                slovaVRadku = poleSlov[radek].Split(' ');
 
+                if (radek == zachytRadek)
+                    hledaneSlovo = slovaVRadku[slovo];
+
+                if (pouzitaSlova.Contains(hledaneSlovo) == false)
+                {
+                    if (poleSlov[radek].Contains(hledaneSlovo))
+                    {
+                        for (int i = 0; i < slovaVRadku.Length; i++)
+                        {
+                            if (hledaneSlovo.Equals(slovaVRadku[i]))
+                            { Console.WriteLine($"{hledaneSlovo} - {radek}"); break; }
+                        }
+                    }
+                    radek++;
+                    if (radek == poleSlov.Length)
+                    {
+                        pouzitaSlova.Add(hledaneSlovo);
+                        radek = 0;//////////////Nesmí se začít od řádku 0, ale od toho dalšího
+                        if (slovo <= slovaVRadku.Length)
+                        {  slovo++; radek = zachytRadek; }
+                        else { slovo = 0; radek = ++zachytRadek; }
+                    }
+                }
+            }
         }
     }
 }
