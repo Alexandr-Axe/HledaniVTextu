823f813277396d73258a1c46dc177609f19b5133
Sekera Alexandr <sekera.alexandr.2017@skola.ssps.cz>
2021-04-22 21:53:53 +0200

    Přepsaný a funkční kód (neokomentovaný)

--- HledaniVTextu/Program.cs
+++ HledaniVTextu/Program.cs
@@ -11,47 +11,51 @@ namespace HledaniVTextu
     {
         static void Main()
         {
-            string path = "testovaci.txt";
-            Words.CreateList(path);
+            string path = "check.txt";
+            Words Slova = new Words();
+            Slova.CreateList(path, "testovaci.txt");
         }
     }
-    public class Words 
+    public class Words
     {
-        public static void CreateList(string path)
+        Dictionary<string, List<int>> seznamSlov = new Dictionary<string, List<int>>(); //Vytvářím si seznam slov, která mají řádky jako list integerů
+        void ReadLine(string path)
         {
-            HashSet<string> pouzitaSlova = new HashSet<string>();
-            string[] poleSlov = File.ReadAllLines(path);
-            string[] slovaVRadku;
-            int radek = 0;
-            int zachytRadek = 0;
-            int slovo = 0;
-            string hledaneSlovo = string.Empty;
-            while (radek != poleSlov.Length)
+            using (StreamReader sr = new StreamReader(path)) //Používáme using pro jednodušší práci (abych nemusel sám odchytávat, kdy se dostanu na konec souboru)
             {
-                slovaVRadku = poleSlov[radek].Split(' ');
-
-                if (radek == zachytRadek)
-                    hledaneSlovo = slovaVRadku[slovo];
-
-                if (pouzitaSlova.Contains(hledaneSlovo) == false)
+                int radek = 0; //Řádek, na kterém se nacházím
+                string slovo = string.Empty;
+                while (sr.EndOfStream.Equals(false)) //Dokud se nedostanu na konec souboru, budu číst řádky
                 {
-                    if (poleSlov[radek].Contains(hledaneSlovo))
+                    radek++; //Posunul jsem se o řádek
+                    string[] slovaNaRadku = sr.ReadLine().Split(' '); //Beru si veškerá slova na určitém řádku
+                    for (int i = 0; i < slovaNaRadku.Length; i++) //Dokud se nedostanu na poslední slovo na řádce, proces se opakuje
                     {
-                        for (int i = 0; i < slovaVRadku.Length; i++)
+                        slovo = slovaNaRadku[i].ToLower();
+                        if (seznamSlov.ContainsKey(slovo).Equals(false))
                         {
-                            if (hledaneSlovo.Equals(slovaVRadku[i]))
-                            { Console.WriteLine($"{hledaneSlovo} - {radek}"); break; }
+                            List<int> radky = new List<int>();
+                            radky.Add(radek);
+                            seznamSlov.Add(slovo, radky);
+                        }
+                        else
+                        {
+                            List<int> radkyMezikrok = seznamSlov[slovo];
+                            radkyMezikrok.Add(radek);
+                            seznamSlov[slovo] = radkyMezikrok;
                         }
                     }
-                    radek++;
-                    if (radek == poleSlov.Length)
-                    {
-                        pouzitaSlova.Add(hledaneSlovo);
-                        radek = 0;//////////////Nesmí se začít od řádku 0, ale od toho dalšího
-                        if (slovo <= slovaVRadku.Length)
-                        {  slovo++; radek = zachytRadek; }
-                        else { slovo = 0; radek = ++zachytRadek; }
-                    }
+                }
+            }
+        }
+        public void CreateList(string path, string save) 
+        {
+            ReadLine(path);
+            using (StreamWriter sw = new StreamWriter(save))
+            {
+                foreach (var Item in seznamSlov) //var mi zjednoduší práci a nemusím zapisovat přímo typ proměnné
+                {
+                    sw.WriteLine(Item.Key + " - " + string.Join(", ", Item.Value.Select(x => x.ToString()).ToArray()));
                 }
             }
         }
